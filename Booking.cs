872ac36080fb5d896c5629c7c8573410f6c21a9a
Clone888 <dwclone@gmail.com>
2023-12-11 11:17:03 +0100

    created a new class and a main menu

---
+++ Booking.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace holidaymaker;
+
+public class Booking
+{
+    public string? firstName = string.Empty;
+    public string? lastName = string.Empty;
+    public void New()
+
+    {
+        Console.WriteLine("Skriv namn");
+        firstName = Console.ReadLine();
+        Console.WriteLine("Skriv efternamn");
+        lastName = Console.ReadLine();
+
+
+
+
+
+
+
+        Console.WriteLine("innan break");
+        Console.ReadKey();
+
+    }
+
+    public void Edit()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+
+
+}
+
